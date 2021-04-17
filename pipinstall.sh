b1075ab72b58c2be9111ac831c45290e9b341a7f
emacslisp <java.gdb@gmail.com>
2021-04-17 12:58:57 +1000

    add install script for MainPythonLib

--- pipinstall.sh
+++ pipinstall.sh
@@ -1,2 +1,5 @@
 # put all python3 install third party library here
-sudo pip3.7 install lxml
+pip3 install -e ./MainLibPython
+pip3 install lxml
+pip3 install html2text
+sudo python3 -m pip install html2text 
