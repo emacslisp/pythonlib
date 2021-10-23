0c732358cc551ce6ed4a0995fff973014efd5fd0
emacslisp <java.gdb@gmail.com>
2021-10-24 00:50:59 +1100

    download image

--- requests_book.py
+++ requests_book.py
@@ -14,3 +14,29 @@ print(soup.select('title'))
 site_p = soup.select('p')
 
 print(site_p[0].getText())
+
+
+res = requests.get('https://en.wikipedia.org/wiki/Deep_Blue_(chess_computer)')
+soup = bs4.BeautifulSoup(res.text, "lxml")
+
+for item in soup.select(".toctext"):
+    print(item.getText())
+
+
+res = requests.get("https://en.wikipedia.org/wiki/Deep_Blue_(chess_computer)")
+soup = bs4.BeautifulSoup(res.text, 'lxml')
+
+index = 1
+print(soup.select('.thumbimage'))
+
+for item in soup.select('.thumbimage'):
+    imgUrl = item['src']
+    imageData = requests.get('https:{}'.format(imgUrl))
+    f = open("{}.png".format(index), "wb")
+    f.write(imageData.content)
+    f.close()
+    print(index)
+    index+=1
+    
+
+
