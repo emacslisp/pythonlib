fbe089d0a2d0464e4d70955aa91a73ed1085934b
emacslisp <java.gdb@gmail.com>
2021-10-28 00:34:22 +1100

    requests_book2.py

--- requests_book2.py
+++ requests_book2.py
@@ -8,3 +8,18 @@ res = requests.get(base_url.format(1))
 soup = bs4.BeautifulSoup(res.text, 'lxml')
 
 print(len(soup.select(".product_pod")))
+
+two_star_titles = []
+
+for n in range(1, 51):
+    scrape_url = base_url.format(n)
+    res = requests.get(scrape_url)
+    soup = bs4.BeautifulSoup(res.text, 'lxml')
+    books = soup.select(".product_pod")
+
+    for book in books:
+        if len(book.select('.star-rating.Two')) > 0 :
+            book_title = book.select('a')[1]['title']
+            two_star_titles.append(book_title)
+
+print(two_star_titles)
