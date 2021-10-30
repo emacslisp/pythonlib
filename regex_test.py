9b98616c2c7a3f62a549bdf38721778bbc2cc3a1
emacslisp <java.gdb@gmail.com>
2021-10-31 08:40:29 +1100

    update regex py

--- regex_test.py
+++ regex_test.py
@@ -7,3 +7,15 @@ t="""int foo()
 print(t);
 
 #print( re.sub(r')\n    {',')\n    {\n    xxxx',t,re.M) );
+
+r = re.search(r'cat|dog', 'The cat is here')
+print(r)
+
+r = re.findall(r'...at', 'The cat in the hat went splat')
+print(r)
+
+r = re.findall(r'^\d','1 is a number')
+print(r)
+
+r = re.findall(r'\d$','the number is 2')
+print(r)
