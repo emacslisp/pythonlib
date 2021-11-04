249ec79456cad23dbc82deeea3fe1c3792fa8ad1
emacslisp <java.gdb@gmail.com>
2021-11-05 06:13:10 +1100

    update pdf book

--- pdf_book.py
+++ pdf_book.py
@@ -11,3 +11,24 @@ page_one = pdf_reader.getPage(0)
 page_one_text = page_one.extractText()
 
 print(page_one_text)
+
+pdf_writer = PyPDF2.PdfFileWriter()
+
+pdf_writer.addPage(page_one)
+
+pdf_output = open('wiki2.pdf', "wb")
+pdf_writer.write(pdf_output)
+f.close()
+pdf_output.close()
+
+## extract all page text
+f = open('wiki.pdf', 'rb')
+
+pdf_text = []
+pdf_reader = PyPDF2.PdfFileReader(f)
+
+for num in range(pdf_reader.numPages):
+    page = pdf_reader.getPage(num)
+    pdf_text.append(page.extractText())
+
+print(pdf_text)
